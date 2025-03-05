699bbb6807a4b248737103cfd39beca19f5044b0
sajid210177 <161884984+sajid210177@users.noreply.github.com>
2025-03-06 01:54:37 +0600

    commit message

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { backend_url } from '../App'
+import { backend_url, currency } from '../App'
 import { toast } from 'react-toastify'
 import { TfiPackage } from 'react-icons/tfi'
 
@@ -24,34 +24,66 @@ const Orders = ({token}) => {
     }
   }
 
+  const statusHandler = async (e,orderId)=>{
+    try {
+      const response = await axios.post(backend_url + '/api/order/status', { orderId, status: e.target.value }, {headers:{token}})
+      if(response.data.success){
+        await fetchAllOrders()
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(response.data.message)
+    }
+  }
+
   useEffect(() => {
     fetchAllOrders()
   }, [token])
 
   return (
-    <div>
+    <div className='pl-8 sm:px-8'>
       <h3>All Orders</h3>
-      <div>
+      <div className='flex flex-col gap-4 pt-4'>
         {orders.map((order)=>(
-          <div key={order._id}>
-            <TfiPackage/>
+          <div key={order._id} className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_0.5fr_1fr] gap-4 items-start p-3 text-gray-700 bg-white rounded-lg'>
+            <TfiPackage className='text-3xl text-secondary'/>
             <div>
               <div>
                 {order.items.map((item,index)=>{
                   if(index === order.items.length -1){
-                    return <p key={index}>
-                      <span>Items: </span>
+                    return <p key={index} className='medium-14'>
+                      <span className='text-tertiary'>Item: </span>
                       {item.name} X {item.quantity} <span>"{item.size}"</span>
                     </p>
                   }else{
-                    return <p key={index}>
-                    <span>Items: </span>
+                    return <p key={index} className='medium-14'>
+                    <span className='text-tertiary'>Item: </span>
                     {item.name} X {item.quantity} <span>"{item.size}"</span> ,
                   </p>
                   }
                 })}
               </div>
+              <p className='medium-14'><span className='text-tertiary'>Name: </span>{order.address.firstName + " " + order.address.lastName}</p>
+              <p className='medium-14'><span className='text-tertiary'>Address: </span>
+              <span>{order.address.street + ", "}</span>
+              <span>{order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}</span>
+              </p>
+              <p>{order.address.phone}</p>
             </div>
+            <div>
+              <p className='text-sm'>Items: {order.items.length}</p>
+              <p className='mt-3'>Method: {order.paymentMethod}</p>
+              <p>Payment: {order.payment ? "Done" : "Pending"}</p>
+              <p>Date: {new Date(order.date).toLocaleDateString()}</p>
+            </div>
+            <p className='text-sm font-semibold'>{currency}{order.amount}</p>
+            <select value={order.status} className='medium-14 p-1 ring-1 ring-slate-900/5 rounded max-w-36 bg-primary'>
+              <option value="Order Placed">Order Placed</option>
+              <option value="Packing">Packing</option>
+              <option value="Shipped">Shipped</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
           </div>
         ))}
       </div>
